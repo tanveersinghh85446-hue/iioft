0f06a159ffdf2734590de9c5f45b35517a782710
tanveersinghh85446-hue <tanveersinghh85446@gmail.com>
2026-05-15 13:12:07 +0530

    Optimize JS bundles and improve Lighthouse performance

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// import { visualizer } from 'rollup-plugin-visualizer'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//     visualizer({
-//       open: true,
-//       gzipSize: true,
-//       brotliSize: true
-//     })
-//   ],
-
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           react: ['react', 'react-dom'],
-//           router: ['react-router-dom'],
-//           motion: ['framer-motion'],
-//           icons: ['react-icons']
-//         }
-//       }
-//     }
-//   }
-// })
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
@@ -42,24 +11,23 @@ export default defineConfig({
     visualizer({
       open: true,
       gzipSize: true,
-      brotliSize: true
-    })
+      brotliSize: true,
+    }),
   ],
 
   build: {
-    cssCodeSplit: false,
-
+    cssCodeSplit: true,
     chunkSizeWarningLimit: 1000,
 
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          motion: ['framer-motion'],
-          icons: ['react-icons']
-        }
-      }
-    }
-  }
-})
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          motion: ["framer-motion"],
+          icons: ["react-icons"],
+        },
+      },
+    },
+  },
+});
